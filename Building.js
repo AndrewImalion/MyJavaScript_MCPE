d488543ddb06a26c9ec50659ccf37c6316b3fd9c
BaronChang <fantasyregina@foxmail.com>
2016-09-23 11:02:31 +0800

    array adding
    it still doesn`t work.

--- Building.js
+++ Building.js
@@ -8,8 +8,17 @@ var compass = 0;
 function newLevel()
 {
     print("Building ver1.0 @BaronChang");
-    clientMessage("Welcome to use ¡ìa<BUILDING>");
+    clientMessage("Welcome to use <BUILDING>");
     //add index of buildings here.
+};
+
+function tripelArriesAdd(adding_arry,added_arry)
+{
+    result_arry = [, , ];
+    result_arry[0] = adding_arry[0] + added_arry[0];
+    result_arry[1] = adding_arry[1] + added_arry[1];
+    result_arry[2] = adding_arry[2] + added_arry[2];
+    return result_arry;
 };
 
 function putBlock(point, id, data)
@@ -20,11 +29,11 @@ function putBlock(point, id, data)
 function buildCube(point, id, data, sizes)
 {
     for (i = 0; i <= sizse[0]; i++) {
-        putBlock(point + [sizes[0] - 1, 0, 0], id, data);
+        putBlock(tripelArriesAdd( point , [sizes[0] - 1, 0, 0]), id, data);
         for (j = 0; j <= sizes[1]; j++) {
-            putBlock(point + [0, sizes[1] - 1, 0], id, data);
+            putBlock(tripelArriesAdd(point , [0, sizes[1] - 1, 0]), id, data);
             for (k = 0; k <= sizes[2]; k++) {
-                putBlock(point + [0, 0, sizes[2] - 1], id, data);
+                putBlock(tripelArriesAdd(point ,[0, 0, sizes[2] - 1]), id, data);
             }
         }
     }
@@ -33,24 +42,24 @@ function buildCube(point, id, data, sizes)
 function buildTube(point, id, data, sizes)
 {
     buildCube(point, id, data, sizes);
-    buildCube(point + [1, 0, 1], 0, 0, sizes + [-2, 0, -2]);
+    buildCube(tripelArriesAdd(point ,[1, 0, 1]), 0, 0, sizes + [-2, 0, -2]);
 };
 
-function useItem(x, y, z, itemId, blockId, side)
+function useItem(x, y, z, itemId, blockId, side, itemData, blockData)
 {
     carryingitem[0] = itemId;
-    carryingitem[1] = Player.getCarriedItemData();
+    carryingitem[1] = itemData;
     //set carried item id,to know what the player is carrying.
-    switch (compass) {
-        case 0:; break;
-        case 1: buildCube([x, y, z], carryingitem[0], carryingitem[1], [8,8,8]); break;
-            //add new buildings here.
-    }
-
+    if (compass == 1) {
+        putBlock([x + 1, y + 1, z + 1], itemId, itemData);
+        buildCube([x, y, z], itemId, itemData, [2, 2, 2]);
+        clientMessage("FINISHED BUILDING 1");
+    };
+    //call new buildings here
 };
 
 function procCmd(cmd)
 {
-    compass = cmd;
-    clientMessage("Now,the compass is¡ìc" + compass);
+    compass = Number(cmd);
+    clientMessage("Now, the compass is " + compass);
 };
