5a2f9ed1d458781b33fe14451bc5d194e542bd0a
BaronChang <fantasyregina@foxmail.com>
2017-05-17 22:30:11 +0800

    Update PLAN_newbld.js
    finished core function!

--- PLAN_newbld.js
+++ PLAN_newbld.js
@@ -8,14 +8,24 @@
   ============================================*/
   
   //core function=============================
-  function putBlock(pt,blc)  /*we use a 3dime arry to implemize a position as this[x,y,y]
-                               use a 2dime arry to implemize a type of block as this [blockId,blockData]*/
-  {
-    setTile(pt[0],pt[1],pt[2],blc[0],blc[1]);
-  };
-  
-  function putCube(pt,blc,size,orient)
-  {
-    if(orient===undefined){orient=0};
-    
-  }
+  function putCube(pt, blc, size, orient) {
+    if (orient === undefined) { orient = 0 };
+    switch (orient) {
+        case 0: {
+            for (var i = 0; i < size[0]; i++) {
+                putBlock([pt[0] + i, pt[1], pt[2]], blc);
+                for (var j = 0; j < size[1]; j++) {
+                    putBlock([pt[0] + i, pt[1] + j, pt[2]], blc);
+                    for (var k = 0; k < size[2]; k++) {
+                        putBlock([pt[0] + i, pt[1] + j, pt[2] + k], blc);
+                    }
+                }
+            }
+        }; break;
+        case 1: putCube([pt[0], pt[1], pt[2] - size[0]], blc, [size[2], size[1], size[0]], 0); break;
+        case 2: putCube([pt[0] - size[0], pt[1], pt[2] - size[2]], blc, size, 0); break;
+        case 3: putCube([pt[0] - size[2], pt[1], pt[2]], blc, [size[2], size[1], size[0]], 0); break
+            //what a fucking talent i am !
+        default: print("Error, Illegal orient!"); break;
+    };
+};
